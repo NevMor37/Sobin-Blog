851212d51299b48c9c1690aaca52de6326db34d7
ShubinWu <swu17@student.gsu.edu>
2018-06-11 09:37:58 -0400

    title change

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ app.use(require('express-formidable')({
 
 // 设置模板全局常量
 app.locals.blog = {
-  title: pkg.name,
+  //title: pkg.name,
+  title: "Me and My Babe's Secret Channel",
   description: pkg.description
 };
 
